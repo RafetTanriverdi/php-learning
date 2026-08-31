a9727d0d5019197150196e344c9540aed57c52b2
RafetTanriverdi <by.rafet.tanriverdi@gmail.com>
2026-08-31 16:06:45 +0300

    pagination

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,44 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
-use OpenApi\Attributes as OA;
 
 class ProductController extends Controller
 {
-
-    public function index()
+    public function index(Request $request)
     {
-        return response()->json([
-            Product::all()
-        ]);
+
+        $perPage = $request->integer('per_page', 10);
+
+        $perPage = min($perPage, 100);
+        $products = Product::paginate($perPage);
+
+        return ProductResource::collection(
+            $products
+        );
     }
 
-
-
-    public function show(int $id)
+    public function show(Product $product)
     {
-        $product = Product::find($id);
-        if (!$product) {
-            return response()->json(
-                [
-                    'message' => "Urun bulunamadi"
-                ],
-                404
-            );
-        }
+
+        return new ProductResource($product);
+    }
+
+    public function store(StoreProductRequest $request)
+    {
+
+        $product = Product::create(
+            $request->validated()
+        );
+
+        return response()->json($product, 201);
+    }
+
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+        $product->update($request->validated());
+
         return response()->json($product);
     }
 
-    public function  store(Request $request)
+    public function destroy(Product $product)
     {
-        $product = Product::create([
-            'name' => $request->name,
-            'price' => $request->price,
-            'stock' => $request->stock,
-            'description' => $request->description,
+
+        $product->delete();
+
+        return response()->json([
+            'message' => ' Product Deleted',
         ]);
-        return response()->json($product, 201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,7 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/products/{product}', [ProductController::class, 'show']);
 Route::post('/products', [ProductController::class, 'store']);
+Route::patch('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
